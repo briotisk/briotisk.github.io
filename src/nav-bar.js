3b189db89f893b26645c2f4244218406fe31f177
briotisk <briotisk@gmail.com>
2024-04-21 14:01:15 -0300

    Estilização finalizada. Falta apenas adicionar imagem e descrição a dois projetos e, em seguida comentar e organizar melhor o código

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,6 +1,12 @@
 const navbar = document.getElementById('nav-bar')
 const navbarClass = navbar.classList
 
+if (window.scrollY == 0){
+    if(navbarClass.contains('nav-bar-style')){
+        navbarClass.remove('nav-bar-style')
+    }
+}
+
 window.addEventListener('scroll', ()=>{
     if (window.scrollY >= 700){
         if(!navbarClass.contains('nav-bar-style')){
